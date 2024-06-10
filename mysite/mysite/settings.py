677bd8818499904e5155477cfd40c081c1281b74
Yasin Rafin <44867848+YasinRafin@users.noreply.github.com>
2024-06-10 17:13:52 +0600

    done vercel settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-xm*!#zdvp7hn6t!6#a#k4vz3f&hgy2n9#45e=%6e21b4ynj5b5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -128,3 +128,7 @@ MEDIA_URL='/pictures/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
