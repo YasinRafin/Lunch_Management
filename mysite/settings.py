bbe56a17aeb0bc481bbf46d575eea4df301f2140
Yasin Rafin <44867848+YasinRafin@users.noreply.github.com>
2024-06-11 11:01:16 +0600

    Done

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,17 +77,17 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-DATABASES={
-    "default":dj_database_url.parse(os.environ.get("DATABASE_URL"))
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+# DATABASES={
+#     "default":dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
-
+DATABASES["default"]=dj_database_url.parse("postgres://testdb_6q0e_user:NkoH1mbgl5is4YSYUieLugNu91Zc54Sy@dpg-cpjjdh2cn0vc73an6tk0-a.oregon-postgres.render.com/testdb_6q0e")
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
